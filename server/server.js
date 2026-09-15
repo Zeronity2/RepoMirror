a4e14f40e1aa128d353256e4411d7fc057a4575e
Khushi Patel <khushiwonder@gmail.com>
2026-09-15 10:16:30 +0530

    fix server port for deployment

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const analyzeRoutes = require("./routes/analyzeRoutes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
